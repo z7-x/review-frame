c0c4f3c9843b17bfe9a9d547d27d79ae64d9c726
z7 <40>
2023-04-28 11:05:30 +0800

    fix:pom文件调整

---
+++ z7-data-stats/data-stats-service/src/main/java/com/z7/bespoke/DataStatsApplication.java
@@ -0,0 +1,22 @@
+//package com.z7.bespoke;
+//
+///**
+// * 项目名称：review-frame
+// * 类 名 称：DataStatsApplication
+// * 类 描 述：TODO 数据分析启动类
+// * 创建时间：2023/4/28 4:00 下午
+// *
+// * @author z7
+// */
+//@SpringBootApplication
+//public class DataStatsApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication application = new SpringApplication(UserAuthApplication.class);
+//        /*Banner.Mode.OFF:关闭;*/
+//        /*Banner.Mode.CONSOLE:控制台输出，默认方式;*/
+//        /*Banner.Mode.LOG:日志输出方式;*/
+//        application.setBannerMode(Banner.Mode.LOG);
+//        application.run(args);
+//    }
+//}
--- z7-gateway/src/main/java/com/z7/bespoke/GatewayApplication.java
+++ z7-gateway/src/main/java/com/z7/bespoke/GatewayApplication.java
@@ -2,6 +2,7 @@ package com.z7.bespoke;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
