2b57a5bc4163dfbff8befd7b67d133c98396841f
z7 <40>
2023-05-11 15:54:18 +0800

    fix:md备注

--- z7-user-auth/src/main/java/com/z7/bespoke/controller/Dept1Controller.java
+++ z7-user-auth/src/main/java/com/z7/bespoke/controller/Dept1Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,4 +32,5 @@ public class Dept1Controller {
     public List<Dept> get() {
         return deptService.getDeptAll();
     }
+
 }
